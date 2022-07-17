9f4af540e7d4f6914be7f2e6c2b814cd48bcc781
annayudina <yudina.anna.zi15@gmail.com>
2022-07-17 15:28:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketSum = 23874;
+        double bonus = ticketSum/20;
+        System.out.println(bonus);
+    }
+}
