cff7ff2f6ad1b717ce18d87ec827455f1c159997
annayudina <yudina.anna.zi15@gmail.com>
2022-08-06 22:10:02 +0300

    BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double ticketSum = 23874;
-        double bonus = ticketSum/20;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
